cb846a51a803545e5106aa9367d0524ea06220dd
abdel42 <abdel.madjid.brahimi@etu.univ-st-etienne.fr>
2019-02-09 11:27:37 +0100

    start

---
+++ src/Info.java
@@ -0,0 +1,45 @@
+public class Info {
+    /**
+     * Classe "interface" qui nous servira à stocker les informations trouvées
+     */
+    private String sujet;
+    private String predicat;
+    private String objet;
+
+    public Info(String sujet, String predicat, String objet) {
+        this.sujet = sujet;
+        this.predicat = predicat;
+        this.objet = objet;
+    }
+
+    public Info(String sujet, String predicat) {
+        this.sujet = sujet;
+        this.predicat = predicat;
+    }
+
+
+    public String getSujet() {
+        return sujet;
+    }
+
+    public String getPredicat() {
+        return predicat;
+    }
+
+    public String getObjet() {
+        return objet;
+    }
+
+    public void setSujet(String sujet) {
+        this.sujet = sujet;
+    }
+
+    public void setPredicat(String predicat) {
+        this.predicat = predicat;
+    }
+
+    public void setObjet(String objet) {
+        this.objet = objet;
+    }
+}
+
